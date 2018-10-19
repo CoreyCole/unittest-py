e0c963e37105ede8dc28aa353ee3c71e0947e61a
coreycole <coreyleoc@gmail.com>
2018-10-19 16:05:37 -0700

    fix: tests

--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from src.main import get_cwd
 
@@ -7,6 +8,7 @@ class TestMain(unittest.TestCase):
 
     def test_get_cwd(self):
         print('testing get_cwd()')
+        os.chdir('src')
         current_dir = get_cwd()
         self.assertIsNotNone(current_dir)
         self.assertEqual(current_dir, 'src')
